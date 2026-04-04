f67c1f3890a8075f8edf25be77e7405d4086d4db
Leesiwon2 <49626048+Leesiwon2@users.noreply.github.com>
2026-04-04 18:16:26 +0900

    초기 세팅
    초기 세팅

---
+++ src/main/java/com/spotwo/spotwo/SpotwoApplication.java
@@ -0,0 +1,13 @@
+package com.spotwo.spotwo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpotwoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpotwoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spotwo/spotwo/SpotwoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spotwo.spotwo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpotwoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
